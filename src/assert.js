8a1fc73a957d3132b181fefbb5449fa013a476d5
Steve King <steve@mydev.co>
2013-03-26 15:39:51 +0100

    Add JsDoc of the assertions

--- src/assert.js
+++ src/assert.js
@@ -38,28 +38,76 @@ function makeAssertion(assertionName, argumentsLength, assertion) {
 }
 
 var Assertions = module.exports = {
+
+   /**
+    * Assert that the supplied actual is truthy (ie: not any of false, zero, undefined or null).
+    *
+    * @param {Boolean} actual
+    * @param {String} [message]
+    * @function
+    */
    assert: makeAssertion('assert', 1, function(actual, message) {
       Assertions.assertEquals(!!actual, true, message);
    }),
 
+   /**
+    * Assert that the supplied actual is not undefined, equivalent to using
+    * `assertNotEquals(actual, undefined, message);`.
+    *
+    * @param actual
+    * @param {String} [message]
+    * @function
+    */
    assertExists: makeAssertion('assert exists', 1, function(actual, message) {
       Assertions.assertNotEquals(actual, undefined, message);
    }),
 
+   /**
+    * Assert that the supplied actual is undefined, equivalent to using `assertEquals(actual, undefined, message)`.
+    *
+    * @param actual
+    * @param {String} [message]
+    * @function
+    */
    assertUndefined: makeAssertion('assert undefined', 1, function(actual, message) {
       Assertions.assertEquals(actual, undefined, message);
    }),
 
+   /**
+    * Assert that the supplied actual is null, equivalent to using `assertEquals(actual, null, message)`..
+    *
+    * @param actual
+    * @param {String} [message]
+    * @function
+    */
    assertNull: makeAssertion('assert null', 1, function(actual, message) {
       Assertions.assertEquals(actual, null, message);
    }),
 
+   /**
+    * Assert that the supplied actual and expected values are the same, in the case of non-primitive data
+    * types, they must also be the same reference as each other.
+    *
+    * @param actual
+    * @param expected
+    * @param {String} [message]
+    * @function
+    */
    assertEquals: makeAssertion('assert equals', 2, function(actual, expected, message) {
       if(actual !== expected) {
          throw new AssertionError(actual, expected, message);
       }
    }),
 
+   /**
+    * Assert that the supplied actual and expected values are not the same as each other, in the case of
+    * non-primitive values, equivalent instances that are not the same reference will be treated as not the
+    * same as each other.
+    *
+    * @param {Boolean} actual
+    * @param {String} [message]
+    * @function
+    */
    assertNotEquals: makeAssertion('assert equals', 2, function(actual, expected, message) {
       if(actual === expected) {
          throw new AssertionError(actual, 'not ' + expected, message);
